b3eb2ef79e55cfb6b15c21a0954b583e86009fbf
rajeev <rajeevjan1991@gmail.com>
2025-02-05 11:22:18 +0530

    changes

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,14 +6,12 @@ import Image from "next/image";
 import React , { useRef, useState } from "react";
 
 export default function Home() {
-      const imgInputRef: any = useRef(null);
-
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [img, setImg] = useState('');
-
-    const formData = new FormData();
+  const imgInputRef: any = useRef(null);
     const submitData = async () => {
+        const fileInput : any = document.getElementById("img_file"); // Replace with your HTML element ID
+        const file = fileInput.files[0];
+        const formData = new FormData();
+        formData.append("file", file);
         let response = await fetch('https://dlhub.8aiku.com/dmai/decode-dm/', {
             method: 'POST',
             body: formData,
@@ -29,34 +27,12 @@ export default function Home() {
     return (
         <>
             <h2 className="text-center justify-center flex font-[800] text-3xl my-3">Multi Qr extrat</h2>
-            <input
-                type='text'
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-                placeholder='Enter Post Title'
-                className='bg-black text-white mx-3'
-            />
-
-            <input
-                type='text'
-                value={body}
-                onChange={e => setBody(e.target.value)}
-                placeholder='Enter Post Body'
-                className='bg-black text-white ms-1'
-            />
             <input 
                   type="file" 
                   ref={imgInputRef} 
                   required 
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    if(e.target.files){
-                      const url : string = URL.createObjectURL(e.target.files[0]);
-                       //console.log(url);
-                       setImg(url);
-                       formData.append("file", e.target.files[0], "chris.jpg");
-                       console.log(formData);
-                    }
-                  }}/>
+                  id="img_file"
+                  />
             <button onClick={submitData}>Submit</button>
         </>
     )
